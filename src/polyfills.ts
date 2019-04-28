9e87c482e2075f9786714b90c98d405d7467af84
Sage Wall <sagewall@users.noreply.github.com>
2019-04-28 07:12:28 -0700

    Update mapbox-gl dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -83,3 +83,7 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+/* @mapbox/mapbox-gl-geocoder
+ * Add global to window, assigning the value of window itself.
+ */
+(window as any).global = window;
